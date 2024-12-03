08311c51fb4bbb0a3869b484005e7845b9f057ac
Luca Castri <lucacastri94@gmail.com>
2024-12-03 11:35:31 +0000

    fix

--- setup.py
+++ setup.py
@@ -1,37 +1,43 @@
-from setuptools import setup
-from fpcmci.version import VERSION
+from setuptools import setup, find_packages
+from causalflow.version import VERSION
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("PyPI/long_description.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
 INSTALL_REQUIRES = ["pandas>=1.5.2",
-                    "netgraph>=4.10.2",
+                    "numba>=0.58.1",
+                    "scipy>=1.3.3",
                     "networkx>=2.8.6",
                     "ruptures>=1.1.7",
                     "scikit_learn>=1.1.3",
                     "torch>=1.11.0",
                     "gpytorch>=1.4",
                     "dcor>=0.5.3",
-                    "h5py>=3.7.0",   
+                    "h5py>=3.7.0",
                     "jpype1>=1.5.0",
                     "mpmath>=1.3.0",  
-                    "causalnex>=0.12.1",
-                    "lingam>=1.8.2",
-                    "tigramite>=5.1.0.3",
+                    "causalnex",
+                    "lingam",
                     "pyopencl>=2024.1",
+                    "matplotlib>=3.7.0",
+                    "numpy",
+                    "pgmpy>=0.1.19",
+                    "tigramite>=5.1.0.3",
+                    "rectangle-packer",
+                    "grandalf"
                     ]
 
 setup(
-    name = 'causalflow',
+    name = 'py-causalflow',
     version = VERSION,    
-    description = 'A Python library for Causal Inference in Time-Series domain',
+    description = 'A Collection of Methods for Causal Discovery from Time-series',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/lcastri/causalflow',
     author = 'Luca Castri',
     author_email = 'lucacastri94@gmail.com',
-    packages = ['causalflow', "causalflow.preprocessing", "causalflow.preprocessing.subsampling_methods", "causalflow.basics", "causalflow.selection_methods", "causalflow.graph", "causalflow.random_system"],
+    packages = find_packages(),
     python_requires='>=3',
     install_requires = INSTALL_REQUIRES,
 
